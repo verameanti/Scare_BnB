5f89e83aa811c03a5180b7f8c619dd01bdfa6861
Laurence Sainsbury <laurencesainsbury@gmail.com>
2021-05-31 05:38:37 +0100

    Created a Ghost Model and started (but commented out) the controller & routes, still need to add validations & default values

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_05_31_040430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ghosts", force: :cascade do |t|
+    t.string "name"
+    t.string "era"
+    t.string "category"
+    t.text "description"
+    t.integer "price"
+    t.integer "rating"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
