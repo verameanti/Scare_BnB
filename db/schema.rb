3d17f31f1f3789ab74cd6452d74cc126c61ac61c
Vera Meanti <verameanti@gmail.com>
2021-06-01 11:26:45 +0100

    added userid to ghosts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_01_101056) do
+ActiveRecord::Schema.define(version: 2021_06_01_102558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2021_06_01_101056) do
     t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_ghosts_on_user_id"
   end
 
   create_table "hauntings", force: :cascade do |t|
@@ -46,4 +48,5 @@ ActiveRecord::Schema.define(version: 2021_06_01_101056) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "ghosts", "users"
 end
