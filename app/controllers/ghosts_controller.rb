5744baae74ac45019d6871ae1c277425ef975a8c
Vera Meanti <verameanti@gmail.com>
2021-06-01 15:55:22 +0100

    methods and views for ghosts controller

--- app/controllers/ghosts_controller.rb
+++ app/controllers/ghosts_controller.rb
@@ -1,15 +1,25 @@
 class GhostsController < ApplicationController
 
   def index
+    @ghosts = Ghost.all
   end
 
   def show
+    @ghost = Ghost.find(params[:id])
   end
 
   def new
+    @ghost = Ghost.new
   end
 
   def create
+    @ghost = Ghost.new(ghost_params)
+    @ghost.user = current_user
+    if @ghost.save
+      redirect_to ghost_path(@ghost)
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -21,4 +31,59 @@ class GhostsController < ApplicationController
   def destroy
   end
 
+private
+
+  def ghost_params
+    params.require(:ghost).permit(:name, :era, :category, :description, :price)
+  end
+
 end
+
+
+#   def new
+#     @bookmark = Bookmark.new
+#     @list = List.find(params[:list_id])
+#   end
+
+#   def create
+#     @list = List.find(params[:list_id])
+#     @bookmark = Bookmark.new(bookmark_params)
+#     @bookmark.list = @list
+#     if @bookmark.save
+#       redirect_to list_path(@list)
+#     else
+#       render 'new'
+#     end
+#   end
+
+#   def destroy
+#     @bookmark = Bookmark.find(params[:id])
+#     list = @bookmark.list
+#     @bookmark.destroy
+#     redirect_to list_path(list)
+#   end
+
+# ---------------------
+
+# def index
+#     @lists = List.all
+#   end
+
+#   def show
+#     @list = List.find(params[:id])
+#     @movies = @list.movies
+#   end
+
+#   def new
+#     @list = List.new
+#   end
+
+#   def create
+#     @list = List.new(list_params)
+#     if @list.save
+#       redirect_to list_path(@list)
+#     else
+#       render 'new'
+#     end
+#   end
+
