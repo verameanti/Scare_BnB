10c4b1cda9e6af8b1a7da1dfdc5102a2850bb186
Oliver Kyte <pr.kyteseo@gmail.com>
2021-06-02 11:35:56 +0100

    First attempt at Hauntings Controller

--- app/controllers/hauntings_controller.rb
+++ app/controllers/hauntings_controller.rb
@@ -1,18 +1,27 @@
 class HauntingsController < ApplicationController
 
   def index
+    @hauntings = Haunting.all
   end
 
   def show
-  end
-
-  def new
+    @haunting = Haunting.find(params[:id])
   end
 
   def create
+    @haunting = Haunting.new(params[haunting_params])
+    @haunting.save
   end
 
   def destroy
+    @haunting = Haunting.find(params[:id])
+    @haunting.destroy
+    redirect_to Ghost show
   end
 
+  private
+
+  def haunting_params
+    params.require(:haunting).permit(:start_date, :end_date, :location)
+  end
 end
