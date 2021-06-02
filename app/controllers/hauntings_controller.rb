b11a63f9e62322b8a7ffdf5f272a9071d7d7ea00
Oliver Kyte <pr.kyteseo@gmail.com>
2021-06-02 12:45:35 +0100

    Create hauntings show

--- app/controllers/hauntings_controller.rb
+++ app/controllers/hauntings_controller.rb
@@ -1,18 +1,34 @@
 class HauntingsController < ApplicationController
 
   def index
+    @hauntings = Haunting.all
   end
 
   def show
-  end
-
-  def new
+    @haunting = Haunting.find(params[:id])
   end
 
   def create
+    @ghost = Ghost.find(params[:ghost_id])
+    @haunting = Haunting.new(params[haunting_params])
+    @haunting.user = current_user
+    @haunting.ghost = @ghost
+    if @haunting.save
+      redirect_to haunting_path(@haunting)
+    else
+      render :template => "ghosts/show"
+    end
   end
 
   def destroy
+    @haunting = Haunting.find(params[:id])
+    @haunting.destroy
+    redirect_to Ghost show
   end
 
+  private
+
+  def haunting_params
+    params.require(:haunting).permit(:start_date, :end_date, :location)
+  end
 end
